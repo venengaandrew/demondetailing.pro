7c527c7f5ffd70836a4cca7ad14a09f9e69d1fb6
Andrew Venenga <andrew@venenga.net>
2024-03-11 16:13:22 -0500

    started designing a navbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script setup lang="ts">
+import NavBar from './components/NavBar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <header>
+      <NavBar />
+    </header>
 
-    <div class="wrapper">
-      <HelloWorld msg="Demon Detailing" />
+    <div class="main-container">
+      <main>
+        <TheWelcome />
+      </main>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
@@ -27,13 +28,12 @@ header {
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.main-container {
+  display: flex;
+  justify-content: center;
+}
 
+@media (min-width: 1024px) {
   .logo {
     margin: 0 2rem 0 0;
   }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './assets/main.css'
 
+import '/src/index.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
