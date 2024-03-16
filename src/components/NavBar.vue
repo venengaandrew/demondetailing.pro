9a0c3a74792d50fab956ff1ac0b236878027693d
Andrew Venenga <andrew@venenga.net>
2024-03-16 15:50:33 -0500

    modified backgound color of navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   
 
-<nav class="bg-white dark:bg-black-900 fixed w-full z-20 top-0 start-0 border-b border-black-200 dark:border-black-600">
+<nav class="bg-black dark:bg-black-900 fixed w-full z-20 top-0 start-0 border-b border-black-200 dark:border-black-600">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+  <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Demon Detailing</span>
   </a>
